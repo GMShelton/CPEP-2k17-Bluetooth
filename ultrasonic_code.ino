25d5e94f98338128d3bdb6b261b55f9800566502
GMShelton <gmflugahorn@gmail.com>
2017-08-02 08:17:58 -0400

    Add files via upload

--- ultrasonic_code.ino
+++ ultrasonic_code.ino
@@ -13,14 +13,28 @@
 #define trigPin 13
 #define echoPin 12
 #define led 11 //redLED
+#define MotorLF 7
+#define MotorLB 8
+#define MotorRpwn 5
+#define MotorLpwn 6
+#define MotorRF 4
+#define MotorRB 3
 #define led2 10 //greenLED
 
 void setup() {
   Serial.begin (9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(led, OUTPUT);
-  pinMode(led2, OUTPUT);
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+  pinMode(led,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(MotorLF,OUTPUT);
+  pinMode(MotorRF,OUTPUT);
+  pinMode(MotorLB,OUTPUT);
+  pinMode(MotorRF,OUTPUT);
+  pinMode(MotorLpwn,OUTPUT);
+  pinMode(MotorRpwn,OUTPUT);
+  digitalWrite(MotorRpwn,255);
+  digitalWrite(MotorLpwn,255);
 }
 
 int cm = 1; // centimeters
@@ -39,11 +53,19 @@ void loop() {
   distance = (duration/2) / 29.1;
   if (distance < 20*cm) {  // This is where the LED On/Off happens
     digitalWrite(led,HIGH); // When the Red condition is met, the Green LED should turn off
+    digitalWrite(MotorLF,HIGH);
+    digitalWrite(MotorRF,HIGH);
+    digitalWrite(MotorLB,LOW);
+    digitalWrite(MotorRB,LOW);
   digitalWrite(led2,LOW);
 }
   else {
     digitalWrite(led,LOW);
     digitalWrite(led2,HIGH);
+    digitalWrite(MotorLF,LOW);
+    digitalWrite(MotorRF,LOW);
+    digitalWrite(MotorRB,HIGH);
+    digitalWrite(MotorLB,HIGH);
   }
   if (distance >= 200*cm || distance <= 0*cm){
     Serial.println("Out of range");
